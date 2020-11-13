21ce7aa8ee8d0c61aaaf0032f7d9cc13bc5216ad
Myh45 <54940009+Myh45@users.noreply.github.com>
2020-11-13 03:44:48 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 text = "Деякий текст з голосними"
+print(text)
+words = text.split()
+word = 0
+for i in range(len(words)):
+    if len(words[word]) > len(words[i]):
+        word = i
+print("Найкоротше слово:", words[word])
+
 golosni = "aеєиіїоуюя"
 print("Кількість голосних в тексті:", sum([text.count(i) for i in golosni]))
